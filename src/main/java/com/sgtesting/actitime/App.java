19f1658b6c2b35ee86900fa8967e51eba9855029
sandeshP1 <sandeshp8660@gmail.com>
2022-12-28 17:23:58 +0530

    new changes added here

--- src/main/java/com/sgtesting/actitime/App.java
+++ src/main/java/com/sgtesting/actitime/App.java
@@ -6,6 +6,11 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+
+
       System.out.println( "Sample Test");
+System.out.println( "Sample Test Case1");
+System.out.println( "Sample Test Case2");
+System.out.println( "Sample Test Case3");
     }
 }
